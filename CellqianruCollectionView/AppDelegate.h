482da550916ec23c83773b6a995516279159abf6
jiangtaidi <jiangtd@jiangtddemacbook-air.local>
2016-03-05 16:31:36 +0800

    Initial Commit

---
+++ CellqianruCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CellqianruCollectionView
+//
+//  Created by jiangtd on 16/3/5.
+//  Copyright © 2016年 jiangtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CellqianruCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CellqianruCollectionView
+//
+//  Created by jiangtd on 16/3/5.
+//  Copyright © 2016年 jiangtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
